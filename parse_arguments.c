ccf8a3fd0daf947bea34e4dc687acf50cea5904b
Dinursult <give.me.kitchen@gmail.com>
2025-04-09 11:09:25 +0200

    parsing the arguments 1

---
+++ parse_arguments.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_arguments.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 11:00:47 by dias              #+#    #+#             */
+/*   Updated: 2025/04/09 11:08:45 by dias             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	parse_arguments(char *av, t_data *data)
+{
+	int	i;
+
+	data->num_philos = ft_atoi(av[1]);
+	data->time_to_die = ft_atoi(av[2]);
+	data->time_to_eat = ft_atoi(av[3]);
+	data->time_to_sleep = ft_atoi(av[4]);
+	data->must_eat_count = ft_atoi(av[5]);
+}
---
+++ philosophers.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 10:31:50 by dias              #+#    #+#             */
+/*   Updated: 2025/04/09 10:43:44 by dias             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+typedef struct s_data
+{
+	int				num_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat_count;
+	int				someone_died;
+	int				all_ate_enough;
+	long long		start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	meal_mutex;
+	pthread_mutex_t	death_mutex;
+	t_philo			*philos;
+}				t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	int				left_fork;
+	int				right_fork;
+	int				meals_eaten;
+	int				is_eating;
+	long long		last_meal_time;
+	struct s_data	*data;
+	pthread_t		thread;
+}				t_philo;
+
+typedef struct s_time
+{
+	long long	seconds;
+	long long	microseconds;
+}				t_time;
+
+int	ft_atoi(const char *str)
+
+#endif
