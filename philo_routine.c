50c741e5a27cadcee8cd4e519bc00b9838910662
Dinursult <give.me.kitchen@gmail.com>
2025-04-19 18:53:17 +0200

    norm error fixed

--- philo_routine.c
+++ philo_routine.c
@@ -6,34 +6,33 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 19:36:20 by dias              #+#    #+#             */
-/*   Updated: 2025/04/19 18:09:58 by dias             ###   ########.fr       */
+/*   Updated: 2025/04/19 18:52:57 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*philo_routine(void *arg)
+static void	handle_single_philo(t_philo *philo, t_data *data)
 {
-	t_philo	*philo;
-	t_data	*data;
+	print_status(philo, "has taken left fork");
+	ft_sleep(data->time_to_die);
+	print_status(philo, "died");
+	pthread_mutex_lock(&data->death_mutex);
+	data->someone_died = 1;
+	pthread_mutex_unlock(&data->death_mutex);
+}
 
-	philo = (t_philo *)arg;
-	data = philo->data;
-	if (data->num_philos == 1)
-	{
-		print_status(philo, "has taken left fork");
-		ft_sleep(data->time_to_die);
-		print_status(philo, "died");
-		pthread_mutex_lock(&data->death_mutex);
-		data->someone_died = 1;
-		pthread_mutex_unlock(&data->death_mutex);
-		return (NULL);
-	}
+static void	initialize_philo_time(t_philo *philo, t_data *data)
+{
 	pthread_mutex_lock(&data->meal_mutex);
 	philo->last_meal_time = get_current_time();
 	pthread_mutex_unlock(&data->meal_mutex);
 	if (philo->id % 2 == 0)
 		ft_sleep(10);
+}
+
+static void	philo_life_cycle(t_philo *philo, t_data *data)
+{
 	while (!is_simulation_finished(data))
 	{
 		philo_eat(philo);
@@ -44,5 +43,21 @@ void	*philo_routine(void *arg)
 			break ;
 		philo_think(philo);
 	}
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+	t_data	*data;
+
+	philo = (t_philo *)arg;
+	data = philo->data;
+	if (data->num_philos == 1)
+	{
+		handle_single_philo(philo, data);
+		return (NULL);
+	}
+	initialize_philo_time(philo, data);
+	philo_life_cycle(philo, data);
 	return (NULL);
 }
