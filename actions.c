1a47855eea9e9a243e049035a8c4ae338560b013
Dinursult <give.me.kitchen@gmail.com>
2025-04-19 18:19:06 +0200

    signle philo case fixed

--- actions.c
+++ actions.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 11:15:40 by dias              #+#    #+#             */
-/*   Updated: 2025/04/13 18:31:14 by dias             ###   ########.fr       */
+/*   Updated: 2025/04/19 18:11:29 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	print_status(t_philo *philo, char *message)
 	timestamp = get_current_time() - data->start_time;
 	pthread_mutex_lock(&data->death_mutex);
 	if (!data->someone_died && !data->all_ate_enough)
-		printf("%lld %d %s\n", timestamp, philo->id, message);
+		printf("Time:%lld philo:%d %s\n", timestamp, philo->id, message);
 	pthread_mutex_unlock(&data->death_mutex);
 	pthread_mutex_unlock(&data->print_mutex);
 }
@@ -34,12 +34,6 @@ void	philo_eat(t_philo *philo)
 	data = philo->data;
 	pthread_mutex_lock(&data->forks[philo->left_fork]);
 	print_status(philo, "has taken left fork");
-	if (data->num_philos == 1)
-	{
-		ft_sleep(data->time_to_die + 1);
-		pthread_mutex_unlock(&data->forks[philo->left_fork]);
-		return ;
-	}
 	pthread_mutex_lock(&data->forks[philo->right_fork]);
 	print_status(philo, "has taken right fork");
 	pthread_mutex_lock(&data->meal_mutex);
--- philo_routine.c
+++ philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 19:36:20 by dias              #+#    #+#             */
-/*   Updated: 2025/04/13 19:38:51 by dias             ###   ########.fr       */
+/*   Updated: 2025/04/19 18:09:58 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ void	*philo_routine(void *arg)
 
 	philo = (t_philo *)arg;
 	data = philo->data;
+	if (data->num_philos == 1)
+	{
+		print_status(philo, "has taken left fork");
+		ft_sleep(data->time_to_die);
+		print_status(philo, "died");
+		pthread_mutex_lock(&data->death_mutex);
+		data->someone_died = 1;
+		pthread_mutex_unlock(&data->death_mutex);
+		return (NULL);
+	}
 	pthread_mutex_lock(&data->meal_mutex);
 	philo->last_meal_time = get_current_time();
 	pthread_mutex_unlock(&data->meal_mutex);
