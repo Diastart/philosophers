e47feb3ccf1eb336c98880c57c115a47a8e12fe7
Dinursult <give.me.kitchen@gmail.com>
2025-04-21 17:11:26 +0200

    edge case fixed

--- philo_routine.c
+++ philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 19:36:20 by dias              #+#    #+#             */
-/*   Updated: 2025/04/19 18:52:57 by dias             ###   ########.fr       */
+/*   Updated: 2025/04/21 17:10:59 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ static void	handle_single_philo(t_philo *philo, t_data *data)
 {
 	print_status(philo, "has taken left fork");
 	ft_sleep(data->time_to_die);
-	print_status(philo, "died");
-	pthread_mutex_lock(&data->death_mutex);
-	data->someone_died = 1;
-	pthread_mutex_unlock(&data->death_mutex);
 }
 
 static void	initialize_philo_time(t_philo *philo, t_data *data)
